3739b6ac6886135b86621a26d10b73e110ace1ac
Tom Collingwood <tomcoll@hotmail.co.uk>
2016-11-24 03:03:11 +0000

    added compiled doDisplace.slo

--- scripts/gitaddasimov.sh
+++ scripts/gitaddasimov.sh
@@ -8,7 +8,7 @@ git add book.c pages.c label.c
 git add Makefilebook Makefilelabel Makefilepages Makefile.sh
 #Shader Files
 cd ../shaders
-git add doDisplace.sl randomDisk.osl randomDiskpaper.osl texture.osl
+git add doDisplace.sl randomDisk.osl randomDiskpaper.osl texture.osl doDisplace.slo
 #RIB file
 cd ../RIB
 git add main.rib 
